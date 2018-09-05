b5b9d80658021c301593bebbf158bd10066b613e
Michiel Brentjens <brentjens@astron.nl>
2018-09-05 14:05:08 +0200

    Pylint-suggested code improvements

--- fourierexploration.py
+++ fourierexploration.py
@@ -22,6 +22,7 @@ from dataclasses import dataclass
 
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcol
 import pyfftw
 import pyfftw.interfaces.scipy_fftpack as fftwpack
 import numba
@@ -29,113 +30,113 @@ import ipywidgets
 
 
 
-'''
-Example of good function documentation:
-    """
-    Solve the tensor equation ``a x = b`` for x.
-    It is assumed that all indices of `x` are summed over in the product,
-    together with the rightmost indices of `a`, as is done in, for example,
-    ``tensordot(a, x, axes=b.ndim)``.
-    Parameters
-    ----------
-    a : array_like
-        Coefficient tensor, of shape ``b.shape + Q``. `Q`, a tuple, equals
-        the shape of that sub-tensor of `a` consisting of the appropriate
-        number of its rightmost indices, and must be such that
-        ``prod(Q) == prod(b.shape)`` (in which sense `a` is said to be
-        'square').
-    b : array_like
-        Right-hand tensor, which can be of any shape.
-    axes : tuple of ints, optional
-        Axes in `a` to reorder to the right, before inversion.
-        If None (default), no reordering is done.
-    Returns
-    -------
-    x : ndarray, shape Q
-    Raises
-    ------
-    LinAlgError
-        If `a` is singular or not 'square' (in the above sense).
-    See Also
-    --------
-    numpy.tensordot, tensorinv, numpy.einsum
-    Examples
-    --------
-    #>>> a = np.eye(2*3*4)
-    #>>> a.shape = (2*3, 4, 2, 3, 4)
-    #>>> b = np.random.randn(2*3, 4)
-    #>>> x = np.linalg.tensorsolve(a, b)
-    #>>> x.shape
-    #(2, 3, 4)
-    #>>> np.allclose(np.tensordot(a, x, axes=3), b)
-    #True
-    """
-'''
+# '''
+# Example of good function documentation:
+#     """
+#     Solve the tensor equation ``a x = b`` for x.
+#     It is assumed that all indices of `x` are summed over in the product,
+#     together with the rightmost indices of `a`, as is done in, for example,
+#     ``tensordot(a, x, axes=b.ndim)``.
+#     Parameters
+#     ----------
+#     a : array_like
+#         Coefficient tensor, of shape ``b.shape + Q``. `Q`, a tuple, equals
+#         the shape of that sub-tensor of `a` consisting of the appropriate
+#         number of its rightmost indices, and must be such that
+#         ``prod(Q) == prod(b.shape)`` (in which sense `a` is said to be
+#         'square').
+#     b : array_like
+#         Right-hand tensor, which can be of any shape.
+#     axes : tuple of ints, optional
+#         Axes in `a` to reorder to the right, before inversion.
+#         If None (default), no reordering is done.
+#     Returns
+#     -------
+#     x : ndarray, shape Q
+#     Raises
+#     ------
+#     LinAlgError
+#         If `a` is singular or not 'square' (in the above sense).
+#     See Also
+#     --------
+#     numpy.tensordot, tensorinv, numpy.einsum
+#     Examples
+#     --------
+#     #>>> a = np.eye(2*3*4)
+#     #>>> a.shape = (2*3, 4, 2, 3, 4)
+#     #>>> b = np.random.randn(2*3, 4)
+#     #>>> x = np.linalg.tensorsolve(a, b)
+#     #>>> x.shape
+#     #(2, 3, 4)
+#     #>>> np.allclose(np.tensordot(a, x, axes=3), b)
+#     #True
+#     """
+# '''
 
-@numba.njit('f4[:,:](f4[:,:])', parallel=True)
-def dB(x):
-    """
-    Converts an array of positive, 32 bit floats to dB
-    (10*log10(x)).
+# @numba.njit('f4[:,:](f4[:,:])', parallel=True)
+# def dB(x):
+#     """
+#     Converts an array of positive, 32 bit floats to dB
+#     (10*log10(x)).
 
-    Uses ``numba.njit`` to parallelize the code to keep
-    it fast enough for interactive use. This function is meant to be
-    used as the `scale` argument to the ``replot()`` function.
+#     Uses ``numba.njit`` to parallelize the code to keep
+#     it fast enough for interactive use. This function is meant to be
+#     used as the `scale` argument to the ``replot()`` function.
 
-    Parameters
-    ----------
+#     Parameters
+#     ----------
 
-    x : 2D ndarray of float32
-        The numbers to convert.
+#     x : 2D ndarray of float32
+#         The numbers to convert.
 
-    Returns
-    -------
-    2D ndarray of float32
+#     Returns
+#     -------
+#     2D ndarray of float32
 
-    Examples
-    --------
-    >>> a = np.array([[0.1, 5, 100.0], [3, 0.01, 500]], dtype=np.float32)
-    >>> dB(a)
-    array([[-1.        ,  0.69897   ,  2.        ],
-           [ 0.47712123, -2.        ,  2.69897   ]], dtype=float32)
-    """
-    nrows, ncols = x.shape
-    result: numba.float32[:, :] = np.empty(x.shape, dtype=np.float32)
-    for row in numba.prange(nrows):
-        for col in range(ncols):
-            result[row, col] = np.log10(x[row, col])
-    return result
-
-
+#     Examples
+#     --------
+#     >>> a = np.array([[0.1, 5, 100.0], [3, 0.01, 500]], dtype=np.float32)
+#     >>> dB(a)
+#     array([[-1.        ,  0.69897   ,  2.        ],
+#            [ 0.47712123, -2.        ,  2.69897   ]], dtype=float32)
+#     """
+#     nrows, ncols = x.shape
+#     result: numba.float32[:, :] = np.empty(x.shape, dtype=np.float32)
+#     for row in numba.prange(nrows):
+#         for col in range(ncols):
+#             result[row, col] = np.log10(x[row, col])
+#     return result
 
 
-def linear(x):
-    """
-    Identity function, can be used as `scale` argument to the
-    ``replot()`` function.
 
-    Parameters
-    ----------
-    x : any Python object
 
-    Returns
-    -------
-    x : the same instance
+# def linear(x):
+#     """
+#     Identity function, can be used as `scale` argument to the
+#     ``replot()`` function.
 
-    Examples
-    --------
-    >>> a = []
-    >>> a is linear(a)
-    True
-    >>> b = 'bbb'
-    >>> 'bbb' is linear(b)
-    True
-    >>> np.zeros(3) is linear(np.zeros(3))
-    False
+#     Parameters
+#     ----------
+#     x : any Python object
 
-    Because these are different instances.
-    """
-    return x
+#     Returns
+#     -------
+#     x : the same instance
+
+#     Examples
+#     --------
+#     >>> a = []
+#     >>> a is linear(a)
+#     True
+#     >>> b = 'bbb'
+#     >>> 'bbb' is linear(b)
+#     True
+#     >>> np.zeros(3) is linear(np.zeros(3))
+#     False
+
+#     Because these are different instances.
+#     """
+#     return x
 
 
 
@@ -160,7 +161,7 @@ def min_max_bl_mask(num_y, num_x, min_bl, max_bl):
              Maximum baseline in terms of a fraction of the maximum
              possible baseline (baseline in one of the corners of the
              uv-plane)
-    
+
     Returns
     -------
     Two-dimensional ``numpy.ndarray`` of ``float32`` of shape (num_y,
@@ -210,13 +211,13 @@ def min_max_bl_mask(num_y, num_x, min_bl, max_bl):
     max_r = np.sqrt(mid_x**2 + mid_y**2)
     minf2 = (min_bl*max_r)**2
     maxf2 = (max_bl*max_r)**2
-    for y in numba.prange(num_y):
-        for x in range(num_x):
-            r2 = (x-mid_x)**2 + (y - mid_y)**2
-            if r2 >= minf2 and r2 <= maxf2:
-                mask[y, x] = 1.0
+    for ix_y in numba.prange(num_y):
+        for ix_x in range(num_x):
+            r_squared = (ix_x-mid_x)**2 + (ix_y - mid_y)**2
+            if minf2 <= r_squared <= maxf2:
+                mask[ix_y, ix_x] = 1.0
             else:
-                mask[y, x] = 0.0
+                mask[ix_y, ix_x] = 0.0
     return mask
 
 
@@ -249,7 +250,7 @@ def make_closure(instance):
     >>> fn() is np.zeros(3)
     False
     """
-    def closure_fn(**kwargs):
+    def closure_fn(**_):
         return instance
     return closure_fn
 
@@ -287,10 +288,10 @@ def imread(filename):
            [0.3961, 0.4248, 0.4928, ..., 0.5412, 0.5373, 0.5621],
            [0.3778, 0.4144, 0.5333, ..., 0.5333, 0.5046, 0.5333]],
           dtype=float32)
-    >>> np.set_printoptions(**print_options)    
+    >>> np.set_printoptions(**print_options)
 
     """
-    img = np.array(plt.imread(filename)[:,:,:3].mean(axis=2), dtype='float32')
+    img = np.array(plt.imread(filename)[:, :, :3].mean(axis=2), dtype='float32')
     output = pyfftw.empty_aligned(img.shape, dtype='float32')
     output[:] = img
     return make_closure(output)
@@ -299,6 +300,28 @@ def imread(filename):
 
 @dataclass
 class SimulationOutput:
+    """
+    Simple collection of images and associated uv-planes  for a
+    synthesis imaging simulation.
+
+    Data members
+    ------------
+    image : numpy.ndarray
+            The original image.
+
+    uv_plane : numpy.ndarray
+               The IFFT of ``image``.
+
+    weight : numpy.ndarray
+             weight with which the original uv-plane is
+             multiplied. May be complex valued.
+
+    weighted_uv_plane : numpy.ndarray
+             ``uvplane``*``weight``.
+
+    dirty_image : numpy.ndarray
+                  FFT of weighted_uv_plane.
+    """
     image: np.ndarray
     uv_plane: np.ndarray
     weight: np.ndarray
@@ -310,26 +333,49 @@ def simulate(image, weights):
     '''
     image : 2D numpy.array of floats
 
-    mask : 2D numpy.array of floats
+    weights : 2D numpy.array of floats
     '''
-    w = weights
     uv_plane = fftwpack.fftshift(fftwpack.ifft2(image, threads=4))
-    weighted_uv_plane = pyfftw.empty_aligned(w.shape, dtype='complex64')
-    weighted_uv_plane[:] = w*uv_plane
+    weighted_uv_plane = pyfftw.empty_aligned(weights.shape, dtype='complex64')
+    weighted_uv_plane[:] = weights*uv_plane
     dirty_image = fftwpack.fft2(fftwpack.fftshift(weighted_uv_plane), threads=4).real
     return SimulationOutput(image, uv_plane,
                             weights, weighted_uv_plane,
                             dirty_image)
 
 
+try:
+    import cv2
+    def hsv_to_rgb(hsv):
+        hsv = hsv.copy()
+        hsv[..., 0] *= 360
+        return cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)[..., ::-1]
+except ImportError:
+    def hsv_to_rgb(hsv):
+        return mcol.hsv_to_rgb(hsv)
+
+
+def complex_rgb(complex_ndarray, scale_fn=lambda x: x, perc=(3, 97), gamma=0.7):
+    amps = scale_fn(np.absolute(complex_ndarray))
+    min_amp, max_amp = np.percentile(amps[::20, ::20], perc)
+    amps -= min_amp
+    amps /= (max_amp - min_amp)
+    amps[amps < 0.0] = 0.0
+    amps[amps > 1.0] = 1.0
+    phases = np.angle(complex_ndarray*np.exp(2j*np.pi*0.5))/(2*np.pi)+0.5
+    hsv = np.empty((phases.shape[0], phases.shape[1], 3),
+                   dtype=np.float32)
+    hsv[..., 0] = phases
+    hsv[..., 1] = 1
+    hsv[..., 2] = (amps**gamma)
+    return hsv_to_rgb(hsv) # mcol.hsv_to_rgb(hsv)
 
 
 
-
-class FourierExplorerGui(object):
+class FourierExplorerGui:
     def __init__(self, **kwargs):
         self.fig = plt.figure(**kwargs)
-        self.axes = self.fig.subplots(2,2)
+        self.axes = self.fig.subplots(2, 2)
         self.fig.subplots_adjust(hspace=0, wspace=0)
         self.imshow_artists = ((None, None), (None, None))
 #        self.__name__ = 'FourierExplorerGui'
@@ -357,27 +403,27 @@ class FourierExplorerGui(object):
             ax_tl_img.set_clim(vmin, vmax)
 
         # Original uv plane
-        to_plot = scale(np.absolute(sim_result.uv_plane))
-        vmin, vmax = np.percentile(to_plot[::20, ::20], (0.5, 99.0))
+        to_plot = scale((sim_result.uv_plane))
+        #vmin, vmax = np.percentile(to_plot[::20, ::20], (1.0, 99.0))
         if ax_tr_img is None:
-            ax_tr_img = ax_tr.imshow(to_plot, vmax=vmax, vmin=vmin,
+            ax_tr_img = ax_tr.imshow(to_plot, #vmax=vmax, vmin=vmin,
                                      interpolation='nearest')
             ax_tr.set_xticks([])
             ax_tr.set_yticks([])
         else:
             ax_tr_img.set_data(to_plot)
-            ax_tr_img.set_clim(vmin, vmax)
+            #ax_tr_img.set_clim(vmin, vmax)
 
         # Masked uv plane
-        to_plot = scale(np.absolute(sim_result.weighted_uv_plane))
+        to_plot = scale((sim_result.weighted_uv_plane))
         if ax_br_img is None:
-            ax_br_img = ax_br.imshow(to_plot, vmax=vmax, vmin=vmin,
+            ax_br_img = ax_br.imshow(to_plot, #vmax=vmax, vmin=vmin,
                                      interpolation='nearest')
             ax_br.set_xticks([])
             ax_br.set_yticks([])
         else:
             ax_br_img.set_data(to_plot)
-            ax_br_img.set_clim(vmin, vmax)
+            #ax_br_img.set_clim(vmin, vmax)
 
         # Dirty image
         vmin, vmax = np.percentile(sim_result.dirty_image[::20, ::20], [1, 99])
@@ -391,7 +437,6 @@ class FourierExplorerGui(object):
         else:
             ax_bl_img.set_data(sim_result.dirty_image)
             ax_bl_img.set_clim(vmin, vmax)
-            previous_simresult = sim_result
         self.fig.canvas.draw_idle() # Apparently a bit faster than fig.canvas.draw()
         self.imshow_artists = ((ax_tl_img, ax_tr_img), (ax_bl_img, ax_br_img))
 
@@ -399,14 +444,14 @@ class FourierExplorerGui(object):
     def interact(self, image_functions):
         ipywidgets.interact(
             self.replot,
-            scale={'Linear': linear, 'dB': dB},
+            scale=ipywidgets.fixed(complex_rgb),
+            #{'Linear': linear, 'dB': dB, 'Complex': complex_rgb},
             img_fn=ipywidgets.Dropdown(options=image_functions,
                                        description='Image'),
             min_bl_fraction=ipywidgets.FloatSlider(
                 min=0, max=100, step=0.25, value=0, continuous_update=True),
             max_bl_fraction=ipywidgets.FloatSlider(
                 min=0, max=100, step=0.25, value=100, continuous_update=True))
-        pass
 
 
 
