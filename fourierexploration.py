cf5db592ca2cbc4c95fcb10b6f08d8255cccceb1
Michiel Brentjens <brentjens@astron.nl>
2018-09-15 21:42:03 +0200

    Added point source, shifting, and initial Fourier analysis exercises.

--- fourierexploration.py
+++ fourierexploration.py
@@ -29,6 +29,16 @@ import numba
 import ipywidgets
 
 
+try:
+    import cv2
+    def hsv_to_rgb(hsv):
+        hsv = hsv.copy()
+        hsv[..., 0] *= 360
+        return cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)[..., ::-1]
+except ImportError:
+    def hsv_to_rgb(hsv):
+        return mcol.hsv_to_rgb(hsv)
+
 
 # '''
 # Example of good function documentation:
@@ -73,70 +83,70 @@ import ipywidgets
 #     """
 # '''
 
-# @numba.njit('f4[:,:](f4[:,:])', parallel=True)
-# def dB(x):
-#     """
-#     Converts an array of positive, 32 bit floats to dB
-#     (10*log10(x)).
+@numba.njit('f4[:,:](f4[:,:])', parallel=True)
+def dB(x):
+    """
+    Converts an array of positive, 32 bit floats to dB
+    (10*log10(x)).
 
-#     Uses ``numba.njit`` to parallelize the code to keep
-#     it fast enough for interactive use. This function is meant to be
-#     used as the `scale` argument to the ``replot()`` function.
+    Uses ``numba.njit`` to parallelize the code to keep
+    it fast enough for interactive use. This function is meant to be
+    used as the `scale` argument to the ``replot()`` function.
 
-#     Parameters
-#     ----------
+    Parameters
+    ----------
 
-#     x : 2D ndarray of float32
-#         The numbers to convert.
+    x : 2D ndarray of float32
+        The numbers to convert.
 
-#     Returns
-#     -------
-#     2D ndarray of float32
+    Returns
+    -------
+    2D ndarray of float32
 
-#     Examples
-#     --------
-#     >>> a = np.array([[0.1, 5, 100.0], [3, 0.01, 500]], dtype=np.float32)
-#     >>> dB(a)
-#     array([[-1.        ,  0.69897   ,  2.        ],
-#            [ 0.47712123, -2.        ,  2.69897   ]], dtype=float32)
-#     """
-#     nrows, ncols = x.shape
-#     result: numba.float32[:, :] = np.empty(x.shape, dtype=np.float32)
-#     for row in numba.prange(nrows):
-#         for col in range(ncols):
-#             result[row, col] = np.log10(x[row, col])
-#     return result
-
-
+    Examples
+    --------
+    >>> a = np.array([[0.1, 5, 100.0], [3, 0.01, 500]], dtype=np.float32)
+    >>> dB(a)
+    array([[-1.        ,  0.69897   ,  2.        ],
+           [ 0.47712123, -2.        ,  2.69897   ]], dtype=float32)
+    """
+    nrows, ncols = x.shape
+    result: numba.float32[:, :] = np.empty(x.shape, dtype=np.float32)
+    for row in numba.prange(nrows):
+        for col in range(ncols):
+            result[row, col] = np.log10(x[row, col])
+    return result
 
 
-# def linear(x):
-#     """
-#     Identity function, can be used as `scale` argument to the
-#     ``replot()`` function.
 
-#     Parameters
-#     ----------
-#     x : any Python object
 
-#     Returns
-#     -------
-#     x : the same instance
+def linear(x):
+    """
+    Identity function, can be used as `scale` argument to the
+    ``replot()`` function.
 
-#     Examples
-#     --------
-#     >>> a = []
-#     >>> a is linear(a)
-#     True
-#     >>> b = 'bbb'
-#     >>> 'bbb' is linear(b)
-#     True
-#     >>> np.zeros(3) is linear(np.zeros(3))
-#     False
+    Parameters
+    ----------
+    x : any Python object
 
-#     Because these are different instances.
-#     """
-#     return x
+    Returns
+    -------
+    x : the same instance
+
+    Examples
+    --------
+    >>> a = []
+    >>> a is linear(a)
+    True
+    >>> b = 'bbb'
+    >>> 'bbb' is linear(b)
+    True
+    >>> np.zeros(3) is linear(np.zeros(3))
+    False
+
+    Because these are different instances.
+    """
+    return x
 
 
 
@@ -206,7 +216,8 @@ def min_max_bl_mask(num_y, num_x, min_bl, max_bl):
            [0., 0., 0., 0., 0., 1., 1., 1., 1., 1., 0., 0., 0., 0.]],
           dtype=float32)
     """
-    mask: numba.float32[:, :] = np.empty((num_y, num_x), dtype=np.float32)
+    mask: numba.float32[:, :] = np.empty((num_y, num_x),
+                                         dtype=np.float32)
     mid_y, mid_x = num_y//2, num_x//2
     max_r = np.sqrt(mid_x**2 + mid_y**2)
     minf2 = (min_bl*max_r)**2
@@ -220,6 +231,23 @@ def min_max_bl_mask(num_y, num_x, min_bl, max_bl):
                 mask[ix_y, ix_x] = 0.0
     return mask
 
+
+def fourier_shift(uv_plane, shift_x, shift_y):
+    """
+    
+    """
+    shift_kernel = point_source_image(uv_plane.shape, shift_x, shift_y, dtype='complex64')
+    return uv_plane*fftwpack.fftshift(
+        fftwpack.ifft2(fftwpack.fftshift(shift_kernel), threads=4))
+
+
+
+def point_source_image(shape=(512, 512), shift_x=0, shift_y=0, dtype='float32'):
+    empty = pyfftw.empty_aligned(shape, dtype=dtype)
+    mid_y, mid_x = shape[0]//2, shape[1]//2
+    empty[:] = 0
+    empty[mid_y - shift_y, mid_x - shift_x] = 1.0
+    return empty
 
 
 def make_closure(instance):
@@ -278,23 +306,23 @@ def imread(filename):
     >>> print_options = np.get_printoptions()
     >>> np.set_printoptions(precision=4)
     >>> img().shape
-    (1024, 1024)
+    (512, 512)
     >>> img()
-    array([[0.6052, 0.6405, 0.5869, ..., 0.5007, 0.5869, 0.5346],
-           [0.5882, 0.5621, 0.5987, ..., 0.5647, 0.5046, 0.5556],
-           [0.5569, 0.5556, 0.434 , ..., 0.5242, 0.5281, 0.5346],
+    array([[0.3778, 0.5333, 0.5007, ..., 0.5281, 0.5647, 0.5046],
+           [0.3974, 0.5033, 0.4771, ..., 0.5242, 0.5085, 0.5333],
+           [0.4484, 0.4797, 0.5373, ..., 0.4549, 0.4366, 0.4654],
            ...,
-           [0.3974, 0.4654, 0.5033, ..., 0.5163, 0.5333, 0.5242],
-           [0.3961, 0.4248, 0.4928, ..., 0.5412, 0.5373, 0.5621],
-           [0.3778, 0.4144, 0.5333, ..., 0.5333, 0.5046, 0.5333]],
+           [0.4523, 0.4693, 0.4183, ..., 0.549 , 0.5556, 0.5608],
+           [0.5072, 0.434 , 0.4405, ..., 0.5556, 0.5686, 0.5686],
+           [0.5882, 0.5987, 0.434 , ..., 0.5399, 0.5216, 0.5046]],
           dtype=float32)
     >>> np.set_printoptions(**print_options)
 
     """
-    img = np.array(plt.imread(filename)[:, :, :3].mean(axis=2), dtype='float32')
+    img = np.array(plt.imread(filename)[..., :3].mean(axis=2), dtype='float32')
     output = pyfftw.empty_aligned(img.shape, dtype='float32')
     output[:] = img
-    return make_closure(output)
+    return make_closure(output[::-2, ::2])
 
 
 
@@ -329,35 +357,39 @@ class SimulationOutput:
     dirty_image: np.ndarray
 
 
-def simulate(image, weights):
+def simulate(image, weights, shift_x, shift_y):
     '''
     image : 2D numpy.array of floats
 
     weights : 2D numpy.array of floats
     '''
-    uv_plane = fftwpack.fftshift(fftwpack.ifft2(image, threads=4))
-    weighted_uv_plane = pyfftw.empty_aligned(weights.shape, dtype='complex64')
-    weighted_uv_plane[:] = weights*uv_plane
-    dirty_image = fftwpack.fft2(fftwpack.fftshift(weighted_uv_plane), threads=4).real
+    uv_plane = fftwpack.fftshift(fftwpack.ifft2(fftwpack.fftshift(image), threads=4))
+    weighted_uv_plane = pyfftw.empty_aligned(weights.shape,
+                                             dtype='complex64')
+    weighted_uv_plane[:] = 0
+    weighted_uv_plane[:] = fourier_shift(weights*uv_plane, shift_x, shift_y)
+    dirty_image = np.empty_like(image, dtype='float32')
+    dirty_image[:] = fftwpack.fftshift(fftwpack.fft2(fftwpack.fftshift(weighted_uv_plane), threads=4).real)*np.product(image.shape)**2/(np.absolute(weights).sum())
     return SimulationOutput(image, uv_plane,
                             weights, weighted_uv_plane,
                             dirty_image)
 
 
-try:
-    import cv2
-    def hsv_to_rgb(hsv):
-        hsv = hsv.copy()
-        hsv[..., 0] *= 360
-        return cv2.cvtColor(hsv, cv2.COLOR_HSV2BGR)[..., ::-1]
-except ImportError:
-    def hsv_to_rgb(hsv):
-        return mcol.hsv_to_rgb(hsv)
+
+def normalize_min_max(values, sample_size=1000, perc=(1, 99)):
+    selection = np.random.choice(values.ravel(), sample_size, replace=True)
+    min_x, max_x = np.percentile(selection, perc)
+    if min_x == max_x or min_x >= max_x*(1-1e-4):
+        max_x=values.max()
+        min_x = max_x/2-1
+    return min_x, max_x
+    
 
 
-def complex_rgb(complex_ndarray, scale_fn=lambda x: x, perc=(3, 97), gamma=0.7):
+def complex_rgb(complex_ndarray, scale_fn=lambda x: x,
+                perc=(0.2, 99.8), gamma=0.7, sample_size=1000):
     amps = scale_fn(np.absolute(complex_ndarray))
-    min_amp, max_amp = np.percentile(amps[::20, ::20], perc)
+    min_amp, max_amp = normalize_min_max(amps, sample_size=sample_size, perc=perc)
     amps -= min_amp
     amps /= (max_amp - min_amp)
     amps[amps < 0.0] = 0.0
@@ -378,80 +410,107 @@ class FourierExplorerGui:
         self.axes = self.fig.subplots(2, 2)
         self.fig.subplots_adjust(hspace=0, wspace=0)
         self.imshow_artists = ((None, None), (None, None))
-#        self.__name__ = 'FourierExplorerGui'
 
 
-    def replot(self, scale, img_fn, min_bl_fraction, max_bl_fraction):
+    def replot(self, img_scale, uv_scale, img_fn,
+               min_bl_fraction, max_bl_fraction,
+               shift_x, shift_y):
         ((ax_tl, ax_tr), (ax_bl, ax_br)) = self.axes
         ((ax_tl_img, ax_tr_img), (ax_bl_img, ax_br_img)) = self.imshow_artists
 
         mask = min_max_bl_mask(*(img_fn().shape),
                                min_bl_fraction/100.0,
                                max_bl_fraction/100.0)
-        sim_result = simulate(img_fn(), mask)
+        num_y, num_x = mask.shape
+        sim_result = simulate(img_fn(), mask,
+                              int(round(shift_x*num_x)),
+                              int(round(shift_y*num_y)))
         # Original
-        vmin, vmax = np.percentile(sim_result.image[::20, ::20], [1, 99])
+        to_plot = img_scale(sim_result.image)
+        vmin, vmax = normalize_min_max(to_plot, sample_size=1000, perc=[1, 99])
+        
         if ax_tl_img is None:
             ax_tl_img = ax_tl.imshow(sim_result.image,
                                      cmap=plt.cm.Greys_r,
                                      vmin=vmin, vmax=vmax,
-                                     interpolation='nearest')
+                                     interpolation='nearest',
+                                     origin='lower')
             ax_tl.set_xticks([])
             ax_tl.set_yticks([])
         else:
-            ax_tl_img.set_data(sim_result.image)
+            ax_tl_img.set_data(to_plot)
             ax_tl_img.set_clim(vmin, vmax)
 
         # Original uv plane
-        to_plot = scale((sim_result.uv_plane))
-        #vmin, vmax = np.percentile(to_plot[::20, ::20], (1.0, 99.0))
+        to_plot = complex_rgb(sim_result.uv_plane, scale_fn=uv_scale)
         if ax_tr_img is None:
             ax_tr_img = ax_tr.imshow(to_plot, #vmax=vmax, vmin=vmin,
-                                     interpolation='nearest')
+                                     interpolation='nearest',
+                                     origin='lower')
             ax_tr.set_xticks([])
             ax_tr.set_yticks([])
         else:
             ax_tr_img.set_data(to_plot)
-            #ax_tr_img.set_clim(vmin, vmax)
 
         # Masked uv plane
-        to_plot = scale((sim_result.weighted_uv_plane))
+        to_plot = complex_rgb(sim_result.weighted_uv_plane, scale_fn=uv_scale)
         if ax_br_img is None:
             ax_br_img = ax_br.imshow(to_plot, #vmax=vmax, vmin=vmin,
-                                     interpolation='nearest')
+                                     interpolation='nearest',
+                                     origin='lower')
             ax_br.set_xticks([])
             ax_br.set_yticks([])
         else:
             ax_br_img.set_data(to_plot)
-            #ax_br_img.set_clim(vmin, vmax)
 
         # Dirty image
-        vmin, vmax = np.percentile(sim_result.dirty_image[::20, ::20], [1, 99])
+        to_plot = img_scale(sim_result.dirty_image)
+        vmin, vmax = normalize_min_max(to_plot,
+                                       sample_size=1000, perc=[1, 99])
+        vmax = to_plot.max()
         if ax_bl_img is None:
-            ax_bl_img = ax_bl.imshow(sim_result.dirty_image,
+            ax_bl_img = ax_bl.imshow(to_plot,
                                      cmap=plt.cm.Greys_r,
                                      vmin=vmin, vmax=vmax,
-                                     interpolation='nearest')
+                                     interpolation='nearest',
+                                     origin='lower')
             ax_bl.set_xticks([])
             ax_bl.set_yticks([])
         else:
-            ax_bl_img.set_data(sim_result.dirty_image)
+            ax_bl_img.set_data(to_plot)
             ax_bl_img.set_clim(vmin, vmax)
         self.fig.canvas.draw_idle() # Apparently a bit faster than fig.canvas.draw()
         self.imshow_artists = ((ax_tl_img, ax_tr_img), (ax_bl_img, ax_br_img))
 
 
     def interact(self, image_functions):
+        layout = ipywidgets.Layout(width='80%')
         ipywidgets.interact(
             self.replot,
-            scale=ipywidgets.fixed(complex_rgb),
-            #{'Linear': linear, 'dB': dB, 'Complex': complex_rgb},
+            #scale=ipywidgets.fixed(complex_rgb),
+            img_scale={'Linear': linear, 'dB': dB},
+            uv_scale={'Linear': linear, 'dB': dB},
             img_fn=ipywidgets.Dropdown(options=image_functions,
                                        description='Image'),
             min_bl_fraction=ipywidgets.FloatSlider(
-                min=0, max=100, step=0.25, value=0, continuous_update=True),
+                min=0, max=100, step=0.25, value=0,
+                continuous_update=True,
+                layout=layout),
             max_bl_fraction=ipywidgets.FloatSlider(
-                min=0, max=100, step=0.25, value=100, continuous_update=True))
+                min=0, max=100, step=0.25, value=100,
+                continuous_update=True,
+                layout=layout),
+            shift_x=ipywidgets.FloatSlider(min=-0.5, max=0.5,
+                                           step=1/1024, value=0,
+                                           continuous_update=True,
+                                           layout=layout,
+                                           readout_format='.3f'),
+            shift_y=ipywidgets.FloatSlider(min=-0.5, max=0.5,
+                                           step=1/1024, value=0,
+                                           continuous_update=True,
+                                           layout=layout,
+                                           readout_format='.3f'),
+        )
 
 
 
