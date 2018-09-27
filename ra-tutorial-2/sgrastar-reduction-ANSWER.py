9d3ed0ef3b135be9d6163d1ba1b2be94daab0695
Michiel Brentjens <brentjens@astron.nl>
2018-09-27 12:28:00 +0200

    Rewrote ANSWER in script style

--- ra-tutorial-2/sgrastar-reduction-ANSWER.py
+++ ra-tutorial-2/sgrastar-reduction-ANSWER.py
@@ -1,146 +1,201 @@
-inp importuvfits
+def pause():
+    raw_input('Press <enter> to continue...')
+
 fitsfile = 'p1conc.uvf'
 vis = 'p1conc.MS'
-go importuvfits
+cal_field = '0'
+sgr_field = '1'
 
-inp vishead
-go vishead
+# This is how one would write one's own scripts:
+importuvfits(
+    fitsfile=fitsfile,
+    vis=vis)
 
-inp listobs
-go listobs
+vishead()
 
-inp plotants
-go plotants
+listobs()
 
-inp plotms
-field = '0'
-go plotms
+plotants()
 
+pause()
 
-inp tclean
-field = '0'
-niter = 0
-datacolumn = 'data'
-imsize = [512, 512]
+plotms(
+    vis=vis,
+    field=cal_field)
+pause()
+
 
 freq_hz = 1612e6
-lambda_over_d = 299792458(freq_hz*8685)
+lambda_over_d = 299792458/(freq_hz*8685)
 res_arcsec = lambda_over_d*180*3600/pi
-
 cell = '%.1f arcsec' % (res_arcsec/3.0,)
-imagename = 'calibrator-raw'
-interactive=True
-go tclean
+
+imagename = 'images/calibrator-raw'
+
+tclean(
+    vis=vis,
+    field=cal_field,
+    niter=0,
+    datacolumn='data',
+    imsize=[512, 512],
+    cell=cell,
+    imagename = imagename,
+    interactive=True)
+pause()
+
+viewer(infile = imagename+'.image')
+pause()
+
+viewer(infile = imagename+'.psf')
+pause()
+
+viewer(infile = imagename+'.model')
+pause()
+
+viewer(infile = imagename+'.residual')
+pause()
 
 
-inp viewer
-infile = imagename+'.image'
-go viewer
-
-infile = imagename+'.psf'
-go viewer
-
-infile = imagename+'.residual'
-go viewer
-
-infile = imagename+'.model'
-go viewer
-
-inp gaincal
 caltable = '1748-253.cal'
-smodel = ['1.17 Jy', '0 Jy', '0 Jy', '0 Jy'] # I, Q, U, V
-solint = '300 s'
-calmode = 'ap'
-refant = '19'
-go gaincal
-
-inp plotcal
-xaxis = 'time'
-yaxis = 'phase'
-iteration = 'antenna'
-subplot = 931
-plotrange = [None, None, -180, +180]
-go plotcal
-
-inp plotcal
-yaxis = 'amp'
-plotrange = [None, None, 0.0, 0.25]
-go plotcal
-
-inp plotcal
-yaxis = 'snr'
-plotrange = [None, None, 0.0, 100]
-go plotcal
+gaincal(
+    vis=vis,
+    field=cal_field,
+    caltable=caltable,
+    smodel=['1.17 Jy', '0 Jy', '0 Jy', '0 Jy'], # I, Q, U, V
+    solint='300 s',
+    calmode='ap',
+    refant='19')
+pause()
 
 
-inp applycal
-gaintable = [caltable]
-field = ''
-go applycal
+plotcal(
+    caltable=caltable,
+    xaxis = 'time',
+    yaxis = 'phase',
+    iteration = 'antenna',
+    subplot = 931,
+    plotrange = [None, None, -180, +180])
+pause()
 
-inp plotms
-field='0'
-plotrange = []
-xaxis='uvdist'
-yaxis='amp'
-go plotms
-
-inp tclean
-niter=200
-imagename = 'calibrator'
-datacolumn = 'corrected'
-interactive = True
-go tclean
-
-inp viewer
-infile= imagename+'.image'
-go
+plotcal(
+    caltable=caltable,
+    xaxis = 'time',
+    yaxis = 'amp',
+    iteration = 'antenna',
+    subplot = 931,
+    plotrange = [None, None, 0.0, 0.25])
+pause()
 
 
-inp plotms
-field = '1'
-xaxis = 'uvdist'
-yaxis = 'amp'
-go plotms
+plotcal(
+    caltable=caltable,
+    field=cal_field,
+    xaxis = 'time',    
+    yaxis = 'snr',
+    iteration = 'antenna',
+    subplot = 931,
+    plotrange = [None, None, 0.0, 100])
+pause()
 
-inp tclean
-field = '1'
-niter = 10000
-interactive = True
-imagename = 'Sgr-A-B-array'
-go
+
+applycal(
+    vis=vis,
+    gaintable=[caltable],
+    field='')
+pause()
 
 
 
+plotms(
+    vis=vis,
+    field=cal_field,
+    plotrange = [],
+    xaxis='uvdist',
+    yaxis='amp')
+pause()
 
-inp tclean
-field = '1'
-niter = 20000
-interactive='True'
-weighting='uniform'
-deconvolver='multiscale'
-scales=[0,1,2,4,8,16,32,64,128]
-usemask='auto-multithresh'
-go tclean
+imagename = 'images/calibrator'
 
-
-
-inp tclean
-field = '1'
-niter = 25000
-interactive='True'
-weighting='uniform'
-deconvolver='multiscale'
-scales=[0,1,2,4,8,16,32,64,128]
-usemask='auto-multithresh'
-gridder='wproject'
-wprojplanes=-1
-go tclean
+tclean(
+    vis=vis,
+    field=cal_field,
+    datacolumn='corrected',
+    niter=200,
+    imsize=[512, 512],
+    cell=cell,
+    imagename=imagename,
+    interactive=True)
+pause()
 
 
+viewer(infile=imagename+'.image')
+pause()
+
+viewer(infile=imagename+'.residual')
+pause()
+
+
+plotms(
+    vis=vis,
+    field=sgr_field,
+    xaxis='uvdist',
+    yaxis='amp')
+pause()
+
+
+imagename = 'images/Sgr-A-B-array'
+tclean(vis=vis,
+       field=sgr_field,
+       datacolumn='corrected',
+       cell=cell,
+       imsize=[1024, 1024],
+       niter=10000,
+       interactive=True,
+       imagename=imagename)
+pause()
+
+
+imagename = 'images/Sgr-A-B-array-uvcut'
+tclean(vis=vis,
+       field=sgr_field,
+       datacolumn='corrected',
+       cell=cell,
+       imsize=[1024, 1024],
+       niter=1000,
+       uvrange='>10klambda',
+       interactive=True,
+       imagename=imagename)
+pause()
+
+imagename = 'images/Sgr-A-B-array-msclean'
+tclean(vis=vis,
+       field=sgr_field,
+       datacolumn='corrected',
+       cell=cell,
+       imsize=[1024, 1024],
+       weighting='uniform',
+       deconvolver='multiscale',
+       scales=[0,1,2,4,8,16,32,64,128],
+       usemask='auto-multithresh',
+       niter=20000,
+       interactive=True,
+       imagename=imagename)
+pause()
 
 
 
 
 
+# inp(tclean)
+# field = '1'
+# niter = 30000
+# interactive='True'
+# weighting='briggs'
+# deconvolver='multiscale'
+# scales=[0,1,2,4,8,16,32,64,128]
+# usemask='auto-multithresh'
+# gridder='wproject'
+# wprojplanes=-1
+# imagename = 'images/Sgr-A-B-array-wproj'
+# go(tclean)
 
